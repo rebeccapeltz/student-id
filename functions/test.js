c7991f6151a14c5facc7dda2f4685d5b00c4c712
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-05-02 12:49:36 -0700

    modify test

--- functions/test.js
+++ functions/test.js
@@ -2,13 +2,26 @@ exports.handler = function (event, context, callback) {
   // post only
 
   if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+    return { statusCode: 405, body: "Method Not Allowed!" };
+  } else {
+    return { statusCode: 200, body: "post" };
   }
-  const params = querystring.parse(event.body);
-  const name = params.name || "World";
 
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, ${name}",
-  });
+  // return { statusCode: 200, body: JSON.stringify({ message: "post" }) };
+
+  // const params = querystring.parse(event.body);
+  // const name = params.name || "World";
+
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ message: "Hello, world ${name}" }),
+  // });
+
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     dirname:__dirname,
+  //     files:files,
+  //   })
+  // })
 };
